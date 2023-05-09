a64a74a3db15e0e447808bb46e3e9240a9843953
ousadojunplid <ousadojunplid@gmail.com>
2023-05-09 18:42:05 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,17 @@ module.exports = {
       'main': '#006CA8',
       'main-2': '#0F172A',
       'normal': '#283847',
+      'normal-b': "#0284C7"
     },
     backgroundColor: {
       'main': '#006CA8',
       'main-2': '#FFCD42',
-      'main-3': "#38BDF8"
+      'main-3': "#38BDF8",
+      'main-2-hover': "#FFDA73",
+      'link-hover': '#F8F8F8'
+    },
+    borderColor: {
+      "main": "#0C4A6E"
     },
     boxShadow: {
       'main': '0 4px 24px rgba(0, 0, 0, 0.08)',
