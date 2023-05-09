9069878fbd67dcdb7e88e8a804ba40a103879ef5
ousadojunplid <ousadojunplid@gmail.com>
2023-05-09 13:55:28 -0300

    config-v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,23 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    textColor: {
+      'main': '#006CA8',
+      'main-2': '#0F172A',
+      'normal': '#283847',
     },
+    backgroundColor: {
+      'main': '#006CA8',
+      'main-2': '#FFCD42',
+      'main-3': "#38BDF8"
+    },
+    boxShadow: {
+      'main': '0 4px 24px rgba(0, 0, 0, 0.08)',
+      'main-2': '0 2px 4px rgba(0, 0, 0, 0.08)'
+    },
+    colors: {
+      transparent: 'transparent',
+    }
   },
   plugins: [],
 }
